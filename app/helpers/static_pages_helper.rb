b7e231e7448e37bc88b47a1c682f31b4988d48dc
adisnabawi <adisazizan@gmail.com>
2015-05-17 16:31:34 +0000

    Exercise 4 First Commit

---
+++ app/helpers/static_pages_helper.rb
@@ -0,0 +1,11 @@
+module StaticPagesHelper
+   #Define the title for the pages
+  def full_title(page_title = '')
+    base_title = "Adis App"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+end
